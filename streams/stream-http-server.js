a349cd9dd3e308732412504cab9bb6bd3b66f6a7
Victor Borges LinuxDell <tedborges.atendimento@gmail.com>
2025-11-21 15:08:51 -0300

    aplicando streams, middleware, banco de dados

---
+++ streams/stream-http-server.js
@@ -0,0 +1,26 @@
+import http from 'node:http';
+import { Transform } from 'node:stream';
+
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1;
+
+        console.log(transformed);
+
+        callback(null, Buffer.from(String(transformed)));
+    }
+}
+
+// req = ReadableStream
+// res = WritableStream
+
+const server = http.createServer(async (req, res) => {
+
+    
+
+    console.log(fullStreamContent);
+
+    return res.end(fullStreamContent);
+});
+
+server.listen(3334);
